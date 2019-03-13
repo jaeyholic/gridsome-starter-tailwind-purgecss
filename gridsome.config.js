d9eea98a4594e03dc31335a6f79bb88eaff5f528
Marc FILLEUL <marc@bocameleon.com>
2019-03-13 10:22:36 +0100

    Tailwind CSS and PurgeCSS Configuration

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,27 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: "Gridsome",
   plugins: []
-}
+};
+
+const purgecss = require("@fullhuman/postcss-purgecss");
+const tailwind = require("tailwindcss");
+
+const postcssPlugins = [tailwind("./tailwind.js")];
+
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins
+      }
+    }
+  }
+};
